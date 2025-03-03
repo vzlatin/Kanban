5c302d726feda1ef26201a7f93da69806b1b6382
vzlatin <victorzlatin0@gmail.com>
2025-03-03 23:05:40 +0200

    feat: Add the stats and a add column button

--- fEnd/Kanban/src/components/board/Board.tsx
+++ fEnd/Kanban/src/components/board/Board.tsx
@@ -13,12 +13,29 @@ const Board = () => {
   const columns = useKanbanStore((state) => state.columns).filter((column) =>
     column.boardId === id
   );
+  const numberOfTasks = useKanbanStore((state) => state.tasks).length;
 
   return (
     <>
       {/* <h1>Board: {boardID.boardID}</h1> */}
-      <div className={styles.board}>
-        {columns.map((column) => <Column column={column} key={column.id} />)}
+      <div className={styles["board"]}>
+        <div className={styles["columns"]}>
+          {columns.map((column) => <Column column={column} key={column.id} />)}
+        </div>
+        <div className={styles["stats"]}>
+          <div className={styles["col-number"]}>{columns.length} Columns.</div>
+          <div className={styles["task-number"]}>{numberOfTasks} Tasks.</div>
+          <div className={styles["task-number-unassigned"]}>
+            0 Unnasigned Tasks.
+          </div>
+          <div className={styles["task-number-critical"]}>
+            0 Critical Tasks.
+          </div>
+          <div className={styles["buttons"]}>
+            <button className={styles["add-column"]}>Add a new Column</button>
+            <img src="/column.svg" />
+          </div>
+        </div>
       </div>
     </>
   );

033e1ee27bcf912d879f8d1aa600cda66141029f
vzlatin <victorzlatin0@gmail.com>
2025-04-09 23:03:40 +0300

    refactor+chore: ImageCropper component css -> tw

--- fEnd/Kanban/src/components/dialog/image/ImageCropperDialog.tsx
+++ fEnd/Kanban/src/components/dialog/image/ImageCropperDialog.tsx
@@ -1,6 +1,3 @@
-import localstyles from "./ImageCropperDialog.module.css";
-import styles from "../CustomDialog.module.css";
-
 import { Dialog, DialogPanel } from "@headlessui/react";
 import ImageCropper from "../../image/ImageCropper";
 import { User } from "../../../types/entities";
@@ -11,15 +8,20 @@ type ImageCropperDialogProps = {
   user: User;
 };
 
-const ImageCropperDialog: React.FC<ImageCropperDialogProps> = (
-  { isOpen, onClose, user },
-) => {
+const ImageCropperDialog: React.FC<ImageCropperDialogProps> = ({
+  isOpen,
+  onClose,
+  user,
+}) => {
   return (
-    <Dialog open={isOpen} as="div" className={styles.dialog} onClose={onClose}>
-      <div className={styles["dialog-overlay"]}>
-        <div className={styles["dialog-panel-wrapper"]}>
-          <DialogPanel transition className={localstyles["dialog-panel"]}>
-            <div className={styles["dialog-description"]}>
+    <Dialog open={isOpen} as="div" onClose={onClose} className="absolute z-10">
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-background">
+        <div className="min-h-screen z-10 flex flex-row justify-center items-center p-[1rem]">
+          <DialogPanel
+            transition
+            className="rounded-[0.5rem] w-[35rem] h-[38rem] bg-background border border-dashed border-accent-blue-100 p-[1.5rem] backdrop-blur-[32px] duration-300 ease-out flex flex-col items-center"
+          >
+            <div className="flex flex-row items-center gap-[2rem]">
               <ImageCropper user={user} onClose={onClose} />
             </div>
           </DialogPanel>

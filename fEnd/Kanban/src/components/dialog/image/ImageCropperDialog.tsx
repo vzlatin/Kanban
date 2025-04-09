dda47709ccdc708fad6854d20d7995417a70805f
vzlatin <victorzlatin0@gmail.com>
2025-04-09 23:29:49 +0300

    refactor+chore: ImageCropper component css -> tw

--- fEnd/Kanban/src/components/dialog/image/ImageCropperDialog.tsx
+++ fEnd/Kanban/src/components/dialog/image/ImageCropperDialog.tsx
@@ -19,7 +19,7 @@ const ImageCropperDialog: React.FC<ImageCropperDialogProps> = ({
         <div className="min-h-screen z-10 flex flex-row justify-center items-center p-[1rem]">
           <DialogPanel
             transition
-            className="rounded-[0.5rem] w-[35rem] h-[38rem] bg-background border border-dashed border-accent-blue-100 p-[1.5rem] backdrop-blur-[32px] duration-300 ease-out flex flex-col items-center"
+            className="rounded-[0.5rem] w-[35rem] h-[40rem] bg-background border border-dashed border-accent-blue-100 p-[1.5rem] backdrop-blur-[32px] duration-300 ease-out flex flex-col items-center"
           >
             <div className="flex flex-row items-center gap-[2rem]">
               <ImageCropper user={user} onClose={onClose} />
--- fEnd/Kanban/src/components/image/ImageCropper.tsx
+++ fEnd/Kanban/src/components/image/ImageCropper.tsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from "react";
-import styles from "./ImageCropper.module.css";
 import ReactCrop, {
   centerCrop,
   convertToPixelCrop,
@@ -19,8 +18,8 @@ const MIN_DIMENSION = 150;
 const ASPECT_RATIO = 1;
 
 const ImageCropper: React.FC<ImageCropperProps> = ({ onClose, user }) => {
-  const changeProfilePicture = useKanbanStore((state) =>
-    state.changeUserProfilePic
+  const changeProfilePicture = useKanbanStore(
+    (state) => state.changeUserProfilePic,
   );
 
   const [imgData, setImgData] = useState("");
@@ -45,8 +44,8 @@ const ImageCropper: React.FC<ImageCropperProps> = ({ onClose, user }) => {
       imageElement.src = imgDataString;
 
       imageElement.addEventListener("load", (e) => {
-        const { naturalWidth, naturalHeight } = e
-          .currentTarget as HTMLImageElement;
+        const { naturalWidth, naturalHeight } =
+          e.currentTarget as HTMLImageElement;
         if (naturalHeight < MIN_DIMENSION || naturalWidth < MIN_DIMENSION) {
           setError(
             `The image should be at least ${MIN_DIMENSION} by ${MIN_DIMENSION} pixels`,
@@ -112,76 +111,74 @@ const ImageCropper: React.FC<ImageCropperProps> = ({ onClose, user }) => {
 
   return (
     <>
-      <div className={styles["component-container"]}>
+      <div className="flex flex-col gap-[0.5rem]">
         <input
           type="file"
           accept="image/*"
           name="profile-image"
-          className={styles["image-input"]}
+          className="file:font-medium file:text-[1rem] file:text-primary file:border-none file:rounded-[0.5rem] file:p-[0.5rem] hover:file:cursor-pointer hover:file:bg-primary hover:file:text-background active:file:bg-background active:file:text-primary"
           onChange={onSelectFile}
         />
         {error && (
           <p
             style={{
-              "color": "#860111",
-              "fontFamily": "QuicksandMedium, sans-serif",
+              color: "#860111",
+              fontFamily: "QuicksandMedium, sans-serif",
             }}
           >
             {error}
           </p>
         )}
-        {imgData &&
-          (
-            <div className={styles["crop-container"]}>
-              <ReactCrop
-                crop={crop}
-                keepSelection
-                aspect={ASPECT_RATIO}
-                minWidth={MIN_DIMENSION}
-                onChange={(_pixelCrop, percentCrop) => setCrop(percentCrop)}
+        {imgData && (
+          <div className="w-[100%] h-[100%] flex flex-col items-center gap-[1rem] justify-center overflow-hidden">
+            <ReactCrop
+              crop={crop}
+              keepSelection
+              aspect={ASPECT_RATIO}
+              minWidth={MIN_DIMENSION}
+              onChange={(_pixelCrop, percentCrop) => setCrop(percentCrop)}
+            >
+              <img
+                ref={imageRef}
+                src={imgData}
+                alt="Upload"
+                style={{
+                  maxHeight: "30rem",
+                }}
+                onLoad={onImageLoad}
+              />
+            </ReactCrop>
+            <div className="flex gap-[1rem] items-center mb-[1rem]">
+              <button
+                className="font-medium text-[1rem] text-accent-blue-200 rounded-[0.5rem] border border-accent-blue-200 p-[0.5rem] transition-transform scale-[0.9] duration-100 ease hover:shadow-md hover:scale-[1] hover:cursor-pointer active:shadow-none active:inset-shadow-md w-[6rem]"
+                onClick={() => {
+                  processImage();
+                  onClose();
+                }}
               >
-                <img
-                  ref={imageRef}
-                  src={imgData}
-                  alt="Upload"
-                  style={{
-                    "maxHeight": "30rem",
-                  }}
-                  onLoad={onImageLoad}
-                />
-              </ReactCrop>
-              <div className={styles["crop-buttons"]}>
-                <button
-                  className={styles["crop-button-ok"]}
-                  onClick={() => {
-                    processImage();
-                    onClose();
-                  }}
-                >
-                  Crop
-                </button>
-                <button
-                  className={styles["crop-button-nok"]}
-                  onClick={() => {
-                    onClose();
-                  }}
-                >
-                  Cancel
-                </button>
-              </div>
+                Crop
+              </button>
+              <button
+                className="font-medium text-[1rem] text-danger rounded-[0.5rem] border border-danger p-[0.5rem] transition-transform scale-[0.9] duration-100 ease hover:shadow-md hover:scale-[1] hover:cursor-pointer active:shadow-none active:inset-shadow-md w-[6rem]"
+                onClick={() => {
+                  onClose();
+                }}
+              >
+                Cancel
+              </button>
             </div>
-          )}
+          </div>
+        )}
       </div>
       {crop && (
         <canvas
           ref={canvasRef}
           style={{
-            "display": "none",
-            "width": "150px",
-            "height": "150px",
+            display: "none",
+            width: "150px",
+            height: "150px",
           }}
-        >
-        </canvas>
+        ></canvas>
       )}
     </>
   );
